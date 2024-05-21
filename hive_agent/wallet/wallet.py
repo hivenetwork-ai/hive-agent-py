d6b581d652ae2abe491651b34b7dd96fd63c7bad
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-05-20 22:51:45 -0400

    Set log_cli = true

--- hive_agent/wallet/wallet.py
+++ hive_agent/wallet/wallet.py
@@ -14,8 +14,6 @@ def get_log_level():
     HIVE_AGENT_LOG_LEVEL = os.getenv('HIVE_AGENT_LOG_LEVEL', 'INFO').upper() # Check for env variable on the server and default to INFO if none is provided
     return getattr(logging, HIVE_AGENT_LOG_LEVEL, logging.INFO)
 
-
-
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
