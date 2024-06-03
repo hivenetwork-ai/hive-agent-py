669b4d043be749089d9236016c07cd9bc835aa5f
Tian <xtyao2015@gmail.com>
2024-06-02 22:27:35 -0400

    added else statement for model

--- hive_agent/llm_settings.py
+++ hive_agent/llm_settings.py
@@ -19,5 +19,8 @@ def init_llm_settings():
         Settings.llm = MistralAI(model=model, api_key=os.getenv('MISTRAL_API_KEY'))
     elif "llama" in model:
         Settings.llm = Ollama(model=model, api_key=os.getenv('LLAMA_API_KEY'))
+    else:
+        Settings.llm = OpenAI(model=model)
+
     Settings.chunk_size = 1024
     Settings.chunk_overlap = 20
