fd5645d999a18dcfd33ff5c9f357471965f2d73c
Tian <xtyao2015@gmail.com>
2024-06-03 09:00:16 -0400

    try again

--- test.py
+++
@@ -1,9 +0,0 @@
-from hive_agent import HiveAgent
-from dotenv import load_dotenv
-load_dotenv()
-my_agent = HiveAgent(
-    name="my_agent",
-    functions=[],
-    instruction="your instructions for this agent's goal",
-)
-my_agent.run()
