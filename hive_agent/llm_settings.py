c6ca9c37b20a2206380c52e10def00c53c8d66f0
Tian Yao <xtyao2015@gmail.com>
2024-06-26 13:28:25 -0400

    [HN-253]: apply request_timeout to OpenAI models as well and change config name

--- hive_agent/llm_settings.py
+++ hive_agent/llm_settings.py
@@ -21,18 +21,19 @@ from hive_agent.config import Config
 def init_llm_settings(config):
     model = config.get("model", "model", "gpt-3.5-turbo")
     if "gpt" in model:
-        Settings.llm = OpenAI(model=model)
+        Settings.llm = OpenAI(model=model, request_timeout=config.get("timeout", "llm", 30))
         logging.info(f"OpenAI model selected")
     elif "claude" in model:
         Settings.llm = Anthropic(model=model, api_key=os.getenv("ANTHROPIC_API_KEY"))
         logging.info(f"Claude model selected")
     elif "llama" in model:
-        Settings.llm = Ollama(model="llama3", request_timeout=config.get("timeout", "ollama", 240))
+        Settings.llm = Ollama(model="llama3", request_timeout=config.get("timeout", "llm", 30))
         logging.info(f"Ollama model selected")
     elif "mixtral" or "mistral" in model:
         Settings.llm = MistralAI(model=model, api_key=os.getenv("MISTRAL_API_KEY"))
         logging.info(f"Mistral model selected")
     else:
+        Settings.llm = OpenAI(model=model, request_timeout=config.get("timeout", "llm", 30))
         logging.info(f"Default OpenAI model selected")
 
     Settings.chunk_size = 1024
