766b93bb2d2a237186f9dbe3166e2126ab087bf4
Tushar <tmarathe04@gmail.com>
2024-07-28 23:17:45 -0400

    [HN-270] feat: added HIVE_AGENT_ID to langtrace

--- hive_agent/llm_settings.py
+++ hive_agent/llm_settings.py
@@ -10,6 +10,10 @@ if "LANGTRACE_API_KEY" in os.environ:
 
     langtrace.init(api_key=os.getenv("LANGTRACE_API_KEY"))
 
+if "HIVE_AGENT_ID" in os.environ:
+    from langtrace_python_sdk import inject_additional_attributes
+    user_id = os.getenv('HIVE_AGENT_ID')
+    
 from llama_index.llms.openai import OpenAI
 from llama_index.llms.anthropic import Anthropic
 from llama_index.llms.ollama import Ollama
@@ -21,20 +25,32 @@ from hive_agent.config import Config
 def init_llm_settings(config):
     model = config.get("model", "model", "gpt-3.5-turbo")
     if "gpt" in model:
-        Settings.llm = OpenAI(
-            model=model, request_timeout=config.get("timeout", "llm", 30)
-        )
+        if "HIVE_AGENT_ID" in os.environ:
+            Settings.llm = inject_additional_attributes(lambda: OpenAI(model=model, request_timeout=config.get("timeout", "llm", 30)), {'user_id': user_id})
+        else:
+            Settings.llm = OpenAI(
+                model=model, request_timeout=config.get("timeout", "llm", 30)
+            )
         logging.info(f"OpenAI model selected")
     elif "claude" in model:
-        Settings.llm = Anthropic(model=model, api_key=os.getenv("ANTHROPIC_API_KEY"))
+        if "HIVE_AGENT_ID" in os.environ:
+            Settings.llm = inject_additional_attributes(lambda: Anthropic(model=model, api_key=os.getenv("ANTHROPIC_API_KEY")), {'user_id': user_id})
+        else:
+            Settings.llm = Anthropic(model=model, api_key=os.getenv("ANTHROPIC_API_KEY"))
         logging.info(f"Claude model selected")
     elif "llama" in model:
-        Settings.llm = Ollama(
-            model="llama3", request_timeout=config.get("timeout", "llm", 30)
-        )
+        if "HIVE_AGENT_ID" in os.environ:
+            Settings.llm = inject_additional_attributes(lambda: Ollama(model=model, api_key=os.getenv("LLAMA_API_KEY"), request_timeout=config.get("timeout", "llm", 30)), {'user_id': user_id})
+        else:
+            Settings.llm = Ollama(
+                model="llama3", request_timeout=config.get("timeout", "llm", 30)
+            )
         logging.info(f"Ollama model selected")
     elif "mixtral" or "mistral" in model:
-        Settings.llm = MistralAI(model=model, api_key=os.getenv("MISTRAL_API_KEY"))
+        if "HIVE_AGENT_ID" in os.environ:
+            Settings.llm = inject_additional_attributes(lambda: MistralAI(model=model, api_key=os.getenv("MISTRAL_API_KEY")), {'user_id': user_id})
+        else:
+            Settings.llm = MistralAI(model=model, api_key=os.getenv("MISTRAL_API_KEY"))
         logging.info(f"Mistral model selected")
     else:
         Settings.llm = OpenAI(
