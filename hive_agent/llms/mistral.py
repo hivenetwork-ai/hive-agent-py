ef2608680db9fb5d9efcfdf649830ec95cd1f5e4
umutunsal <42064408+umutunsal@users.noreply.github.com>
2024-09-26 22:57:23 +0300

    Update agent type for claude and mistral

--- hive_agent/llms/mistral.py
+++ hive_agent/llms/mistral.py
@@ -1,5 +1,5 @@
 from llama_index.core.settings import Settings
-from llama_index.core.agent import FunctionCallingAgentWorker
+from llama_index.core.agent import ReActAgentWorker
 
 from hive_agent.llms.llm import LLM
 
@@ -8,7 +8,7 @@ class MistralLLM(LLM):
 
     def __init__(self, llm=None, tools=None, instruction="", tool_retriever=None):
         super().__init__(llm, tools, instruction, tool_retriever)
-        self.agent = FunctionCallingAgentWorker.from_tools(
+        self.agent = ReActAgentWorker.from_tools(
             tools=self.tools,
             system_prompt=self.system_prompt,
             llm=llm, 
