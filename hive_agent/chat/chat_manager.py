13a4f622e29957fdd4f63782e81f0f25e6392e32
Tomisin Jenrola <13075885+ToJen@users.noreply.github.com>
2024-10-28 09:43:40 -0400

    fix: sql read filter

--- hive_agent/chat/chat_manager.py
+++ hive_agent/chat/chat_manager.py
@@ -40,9 +40,9 @@ class ChatManager:
     async def get_messages(self, db_manager: DatabaseManager):
         filters = {"user_id": [self.user_id], "session_id": [self.session_id]}
         if "HIVE_AGENT_ID" in os.environ:
-            filters["agent_id"] = os.getenv("HIVE_AGENT_ID", "")
+            filters["agent_id"] = [os.getenv("HIVE_AGENT_ID", "")]
         if "HIVE_SWARM_ID" in os.environ:
-            filters["swarm_id"] = os.getenv("HIVE_SWARM_ID", "")
+            filters["swarm_id"] = [os.getenv("HIVE_SWARM_ID", "")]
 
         db_chat_history = await db_manager.read_data("chats", filters)
         chat_history = [ChatMessage(role=chat["role"], content=chat["message"]) for chat in db_chat_history]
@@ -51,9 +51,9 @@ class ChatManager:
     async def get_all_chats_for_user(self, db_manager: DatabaseManager):
         filters = {"user_id": [self.user_id]}
         if "HIVE_AGENT_ID" in os.environ:
-            filters["agent_id"] = os.getenv("HIVE_AGENT_ID", "")
+            filters["agent_id"] = [os.getenv("HIVE_AGENT_ID", "")]
         if "HIVE_SWARM_ID" in os.environ:
-            filters["swarm_id"] = os.getenv("HIVE_SWARM_ID", "")
+            filters["swarm_id"] = [os.getenv("HIVE_SWARM_ID", "")]
 
         db_chat_history = await db_manager.read_data("chats", filters)
 
